9a91b23e2c2b90052ee56f0f344a1a8bd57e2708
Mateusz Drużny <druzny141@wp.pl>
2025-01-19 23:10:09 +0100

    added dependiences

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,8 @@ plugins {
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
     alias(libs.plugins.compose.compiler) apply false
 }
+buildscript {
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
