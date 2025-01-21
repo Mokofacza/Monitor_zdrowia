fc0cc5e8f5cb5bd4aa8bdd60697100cd2e626c4a
Szymon <Szym3k636@hotmail.com>
2025-01-21 09:52:20 +0100

    tabela USer w bazy

--- app/src/main/java/temu/monitorzdrowia/data/local/MoodDatabase.kt
+++ app/src/main/java/temu/monitorzdrowia/data/local/MoodDatabase.kt
@@ -1,18 +1,21 @@
 package temu.monitorzdrowia.data.local
 
+import UserDao
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import temu.monitorzdrowia.data.models.Mood
+import temu.monitorzdrowia.data.models.User
 
 // Ten plik definiuje główną klasę bazy danych Room dla aplikacji,
-// która zawiera tabelę Mood oraz konwertery typów niestandardowych.
+// która zawiera tabele Mood oraz User, a także konwertery typów niestandardowych.
 @Database(
-    entities = [Mood::class],
-    version = 1
+    entities = [Mood::class, User::class], // Poprawione dodanie klasy User jako drugiej tabeli
+    version = 1,
+    exportSchema = true // Zalecane, aby zachować schemat bazy danych dla przyszłych migracji
 )
-@TypeConverters(Converters::class)
-abstract class MoodDatabase: RoomDatabase(){
-    abstract val dao: MoodDao // Abstrakcyjna właściwość DAO, Room automatycznie wygeneruje implementację
-
+@TypeConverters(Converters::class) // Informuje Room o niestandardowych konwerterach typów
+abstract class MoodDatabase : RoomDatabase() {
+    abstract val moodDao: MoodDao // Abstrakcyjna właściwość DAO dla Mood
+    abstract val userDao: UserDao // Abstrakcyjna właściwość DAO dla User
 }
--- app/src/main/java/temu/monitorzdrowia/data/models/User.kt
+++ app/src/main/java/temu/monitorzdrowia/data/models/User.kt
@@ -3,23 +3,16 @@ package temu.monitorzdrowia.data.models
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.time.LocalDate
-import java.time.Period
+
 
 // Ten plik jest odpowiedzialny za opisanie użytkownika w bazie danych.
 // To właśnie tu definiujemy, jakie informacje zapisujemy o użytkowniku.
 
-@Entity(tableName = "users") // Mówi Roomowi, że ta klasa to tabela w bazie danych
+@Entity // Mówi Roomowi, że ta klasa to tabela w bazie danych
 data class User(
     val name: String, // Imię użytkownika
     val subname: String, // Nazwisko użytkownika
     val birthDate: LocalDate, // Data urodzenia użytkownika
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0 // Unikalny identyfikator użytkownika, generowany automatycznie
-) {
-    // Wyliczanie wieku użytkownika na podstawie daty urodzenia
-    val age: Int
-        get() {
-            val currentDate = LocalDate.now()
-            return Period.between(birthDate, currentDate).years
-        }
-}
+)
